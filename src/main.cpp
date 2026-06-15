9792a22b3451ba30178a74745dbe28d68ae4f9fc
Something12343213412 <Kevbo2007@icloud.com>
2026-06-15 14:32:13 -0700

    A lot of changes, primarily moving things into different files

--- src/main.cpp
+++ src/main.cpp
@@ -95,7 +95,14 @@ void logData(char* file){
 }
 
 void logAll(){
-    char files[][10] = {"Axis1.txt", "Axis2.txt", "Axis3.txt", "Axis4.txt", "BtnUp.txt"};
+    char files[][10] = 
+    {   "Axis1.txt", "Axis2.txt", "Axis3.txt", "Axis4.txt", 
+        "BtnUp.txt", "BtnDn.txt", "BtnRt.txt", "BtnLt.txt",
+        "BtnAA.txt", "BtnBB.txt", "BtnXX.txt", "BtnYY.txt",
+        "BtnL1.txt", "BtnL2.txt", "BtnR1.txt", "BtnR2.txt"
+    };
+    
+    
     // we take total number of bytes then /10 as each thing has 10 bytes to get total size
     for(int i = 0; i < sizeof(files)/10; i++){
         logData(files[i]);
@@ -124,24 +131,106 @@ void writeButtonUp(){
     writeData("BtnUp.txt", Controller1.ButtonUp.pressing(), Brain.timer(msec));
 }
 
+void writeButtonDown(){
+    writeData("BtnDn.txt", Controller1.ButtonDown.pressing(), Brain.timer(msec));
+}
+
+void writeButtonRight(){
+    writeData("BtnRt.txt", Controller1.ButtonRight.pressing(), Brain.timer(msec));
+}
+
+void writeButtonLeft(){
+    writeData("BtnLt.txt", Controller1.ButtonLeft.pressing(), Brain.timer(msec));
+}
+
+void writeButtonA(){
+    writeData("BtnAA.txt", Controller1.ButtonA.pressing(), Brain.timer(msec));
+}
+
+void writeButtonB(){
+    writeData("BtnBB.txt", Controller1.ButtonB.pressing(), Brain.timer(msec));
+}
+
+void writeButtonX(){
+    writeData("BtnXX.txt", Controller1.ButtonX.pressing(), Brain.timer(msec));
+}
+
+void writeButtonY(){
+    writeData("BtnYY.txt", Controller1.ButtonY.pressing(), Brain.timer(msec));
+}
+
+void writeButtonL1(){
+    writeData("BtnL1.txt", Controller1.ButtonL1.pressing(), Brain.timer(msec));
+}
+
+void writeButtonL2(){
+    writeData("BtnL2.txt", Controller1.ButtonL2.pressing(), Brain.timer(msec));
+}
+
+void writeButtonR1(){
+    writeData("BtnR1.txt", Controller1.ButtonR1.pressing(), Brain.timer(msec));
+}
+
+void writeButtonR2(){
+    writeData("BtnR2.txt", Controller1.ButtonR2.pressing(), Brain.timer(msec));
+}
+
 int main() {
     uint8_t empty[0];
 
     // list of files, make sure they line up with the list in log all
-    char files[][10] = {"Axis1.txt", "Axis2.txt", "Axis3.txt", "Axis4.txt", "BtnUp.txt"};
+    char files[][10] = 
+    {   "Axis1.txt", "Axis2.txt", "Axis3.txt", "Axis4.txt", 
+        "BtnUp.txt", "BtnDn.txt", "BtnRt.txt", "BtnLt.txt",
+        "BtnAA.txt", "BtnBB.txt", "BtnXX.txt", "BtnYY.txt",
+        "BtnL1.txt", "BtnL2.txt", "BtnR1.txt", "BtnR2.txt"
+    };
 
     for(int i = 0; i < sizeof(files)/10; i++){
         Brain.SDcard.savefile(files[i], empty, 0); // empty out all files
     }
 
-    
-    
     Controller1.Axis1.changed(writeAxis1);
     Controller1.Axis2.changed(writeAxis2);
     Controller1.Axis3.changed(writeAxis3);
     Controller1.Axis4.changed(writeAxis4);
+
+    // I hate this solution, I spent so much time trying to pass in callbacks automatically generated but apparently you can't pass in a function from a this pointer which makes sense but makes me angry
     Controller1.ButtonUp.pressed(writeButtonUp);
     Controller1.ButtonUp.released(writeButtonUp);
+
+    Controller1.ButtonDown.pressed(writeButtonDown);
+    Controller1.ButtonDown.released(writeButtonDown);
+
+    Controller1.ButtonRight.pressed(writeButtonRight);
+    Controller1.ButtonRight.released(writeButtonRight);
+
+    Controller1.ButtonLeft.pressed(writeButtonLeft);
+    Controller1.ButtonLeft.released(writeButtonLeft);
+
+    //Controller1.ButtonA.pressed(writeButtonA);
+    //Controller1.ButtonA.released(writeButtonA);
+
+    Controller1.ButtonB.pressed(writeButtonB);
+    Controller1.ButtonB.released(writeButtonB);
+
+    Controller1.ButtonX.pressed(writeButtonX);
+    Controller1.ButtonX.released(writeButtonX);
+
+    Controller1.ButtonY.pressed(writeButtonY);
+    Controller1.ButtonY.released(writeButtonY);
+
+    Controller1.ButtonL1.pressed(writeButtonL1);
+    Controller1.ButtonL1.released(writeButtonL1);
+
+    Controller1.ButtonL2.pressed(writeButtonL2);
+    Controller1.ButtonL2.released(writeButtonL2);
+
+    Controller1.ButtonR1.pressed(writeButtonR1);
+    Controller1.ButtonR1.released(writeButtonR1);
+
+    Controller1.ButtonR2.pressed(writeButtonR2);
+    Controller1.ButtonR2.released(writeButtonR2);
 
 
 
